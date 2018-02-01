2ce07b9dca441b2065b974fa6fb1b00245f1bc76
god <164798848@qq.com>
2018-02-01 13:20:48 +0800

    code

--- server_error.go
+++ server_error.go
@@ -1,16 +1,18 @@
 package restful
 
 import (
-	apiError "github.com/NeuronFramework/errors"
+	neuronError "github.com/NeuronFramework/errors"
 	"github.com/go-openapi/errors"
+	"go.uber.org/zap"
 	"net/http"
 	"strings"
 )
 
 func ServeError(rw http.ResponseWriter, r *http.Request, err error) {
+	zap.L().Named("ServeError").Info("ServeError", zap.Error(err))
 	rw.Header().Set("Content-Type", "application/json")
 	switch e := err.(type) {
-	case *apiError.Error:
+	case *neuronError.Error:
 		rw.WriteHeader(e.Status)
 		rw.Write([]byte(e.Error()))
 		return
@@ -18,7 +20,7 @@ func ServeError(rw http.ResponseWriter, r *http.Request, err error) {
 		rw.Header().Add("Allow", strings.Join(err.(*errors.MethodNotAllowedError).Allowed, ","))
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		if r == nil || r.Method != "HEAD" {
-			apiError := apiError.Error{
+			apiError := neuronError.Error{
 				Status:  http.StatusMethodNotAllowed,
 				Code:    "MethodNotAllowed",
 				Message: "Http method not allowed",
@@ -29,15 +31,15 @@ func ServeError(rw http.ResponseWriter, r *http.Request, err error) {
 	case errors.Error:
 		if e == nil {
 			rw.WriteHeader(http.StatusInternalServerError)
-			apiError := apiError.Unknown("e==nil")
+			apiError := neuronError.Unknown("e==nil")
 			rw.Write([]byte(apiError.Error()))
 			return
 		}
 		rw.WriteHeader(int(e.Code()))
 		if r == nil || r.Method != "HEAD" {
-			apiError := apiError.Error{
+			apiError := neuronError.Error{
 				Status:  int(e.Code()),
-				Code:    apiError.ErrUnknown,
+				Code:    neuronError.ErrUnknown,
 				Message: e.Error(),
 			}
 			rw.Write([]byte(apiError.Error()))
@@ -46,7 +48,7 @@ func ServeError(rw http.ResponseWriter, r *http.Request, err error) {
 	default:
 		rw.WriteHeader(http.StatusInternalServerError)
 		if r == nil || r.Method != "HEAD" {
-			apiError := apiError.Unknown(e.Error())
+			apiError := neuronError.Unknown(e.Error())
 			rw.Write([]byte(apiError.Error()))
 		}
 	}
