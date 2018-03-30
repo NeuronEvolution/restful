2f6f5dc14eb8d4256b2abbc6862ce77b5cbba280
god <164798848@qq.com>
2018-03-30 10:13:26 +0800

    code

---
+++ context.go
@@ -0,0 +1,33 @@
+package restful
+
+import (
+	"net/http"
+	"time"
+)
+
+type Context struct {
+	UserAgent string
+}
+
+func (ctx *Context) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (ctx *Context) Done() <-chan struct{} {
+	return nil
+}
+
+func (ctx *Context) Err() error {
+	return nil
+}
+
+func (ctx *Context) Value(key interface{}) interface{} {
+	return nil
+}
+
+func NewContext(r *http.Request) (ctx *Context) {
+	ctx = &Context{}
+	ctx.UserAgent = r.UserAgent()
+
+	return ctx
+}
